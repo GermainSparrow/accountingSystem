fdede0da21371f1c0abf27a230c159baf90c1e02
sam.lai <sam.lai@silksoftware.com>
2023-03-15 10:22:13 +0800

    配置了一下打包设置

--- accountingSystemBack/src/db.js
+++ accountingSystemBack/src/db.js
@@ -2,19 +2,19 @@ const mysql = require('mysql')     //引入mysql 模块
 // 创建数据库连接 填入数据库信息 
 
 //远程数据库
-const db = mysql.createConnection({
-    user: 'root',          //用户名
-    password: '200113Sam_lai',	//密码
-    host: '1.14.74.199',		//主机（默认都是local host）
-    database: 'xialai'       //数据库名
-})
-//本地数据库
 // const db = mysql.createConnection({
 //     user: 'root',          //用户名
 //     password: '200113Sam_lai',	//密码
-//     host: '127.0.0.1',		//主机（默认都是local host）
-//     database: 'xiaolai'       //数据库名
+//     host: '1.14.74.199',		//主机（默认都是local host）
+//     database: 'xialai'       //数据库名
 // })
+//本地数据库
+const db = mysql.createConnection({
+    user: 'root',          //用户名
+    password: '200113Sam_lai',	//密码
+    host: '127.0.0.1',		//主机（默认都是local host）
+    database: 'xiaolai'       //数据库名
+})
 // 测试连接
 db.connect(err => {
     console.log(err == null ? '服务器连接成功' : '服务器连接失败,err---->' + err);
--- accountingWeb/vite.config.ts
+++ accountingWeb/vite.config.ts
@@ -4,4 +4,17 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 1000, // 以 KB 为单位，调整 chunk size 的警告阈值
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          react: ['react'],
+          'react-dom': ['react-dom'],
+        },
+      },
+    },
+    outDir: 'dist', // 输出目录
+  },
 })
+
