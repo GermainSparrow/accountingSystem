3daa61c26ed440fb4ec8197dde06409439e9645f
sam.lai <sam.lai@silksoftware.com>
2023-03-20 10:10:49 +0800

    安装mysql 配置跨域 mysql设置

--- eggBack/config/config.default.js
+++ eggBack/config/config.default.js
@@ -12,6 +12,24 @@ module.exports = appInfo => {
    **/
   const config = exports = {};
 
+  // CSRF //允许跨域请求
+  config.security = {
+    csrf: {
+      enable: false,
+    },
+  };
+  //配置mysql
+  config.mysql = {
+    app: true,
+    agent: false,
+    client: {
+      host: '127.0.0.1',
+      port: '3306',
+      user: 'root',
+      password: '200113',
+      database: 'xiaolai'
+    }
+  }
   // use for cookie sign key, should change to your own and keep security
   config.keys = appInfo.name + '_200113';
 
--- eggBack/config/plugin.js
+++ eggBack/config/plugin.js
@@ -1,9 +1,9 @@
 'use strict';
-
-/** @type Egg.EggPlugin */
-module.exports = {
-  // had enabled by egg
-  // static: {
-  //   enable: true,
-  // }
-};
+exports.ejs = {
+  enable: true,
+  package: 'egg-view-ejs',
+}
+exports.mysql = {
+  enable: true,
+  package: 'egg-view-ejs',
+}
