d7551e68adcd836c72eae4d83392df78f1fa98c7
sam.lai <sam.lai@silksoftware.com>
2023-03-21 08:28:39 +0800

    优化一下登录 把密码删掉传回前端

--- eggBack/app/controller/user.js
+++ eggBack/app/controller/user.js
@@ -5,6 +5,7 @@ const { Controller } = require('egg');
 class userControl extends Controller {
     async login() {
         const data = await this.service.user.login(this.ctx.request.body)
+        delete data.password;
         data ? this.ctx.body = { 'code': 200, data } : this.ctx.body = { 'code': 500, data }
     }
 }
