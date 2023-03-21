5bc30a7200cf5fa721a414e3c390c646b24bc270
sam.lai <sam.lai@silksoftware.com>
2023-03-21 11:28:51 +0800

    '设置了token的过期时间'

--- eggBack/app/extend/helper.js
+++ eggBack/app/extend/helper.js
@@ -1,7 +1,7 @@
 //token校验函数
 module.exports = {
     getToken(value) {
-        return this.app.jwt.sign(value, this.app.config.jwt.secret);
+        return this.app.jwt.sign({ userId: value, exp: 60 * 60 * 7 }, this.app.config.jwt.secret);
     }
 }
 
