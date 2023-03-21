af965c23ffea7525408689a1e444a505586a754f
sam.lai <sam.lai@silksoftware.com>
2023-03-21 08:22:33 +0800

    解决登录接口bug

--- eggBack/app/service/user.js
+++ eggBack/app/service/user.js
@@ -4,10 +4,9 @@ const Service = require('egg').Service;
 
 class Test extends Service {
     async login(obj) {
-        console.log(obj);
         try {
             const result = await this.app.mysql.select('user', { where: { name: obj.userName, password: obj.password } })
-            return result > 0 ? result : false;
+            return result.length > 0 ? result[0] : false;
         } catch (err) {
             console.log('err---------------->', err);
             return false;
