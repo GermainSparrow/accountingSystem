ace38ecaac19536c751f90ae6676bc6add230cd6
sam.lai <sam.lai@silksoftware.com>
2023-05-04 00:42:07 +0800

    fix: os not find

--- eggBack/app.js
+++ eggBack/app.js
@@ -2,7 +2,7 @@ const egg = require('egg');
 const os = require('os');
 
 egg.startCluster({
-  baseDir: __dirname,
-  workers: os.cpus().length,
-  port: process.env.PORT || 7001,
+    baseDir: __dirname,
+    workers: os.cpus().length,
+    port: process.env.PORT || 7001,
 });
